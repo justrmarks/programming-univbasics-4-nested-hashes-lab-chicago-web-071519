9cb7be298aeb319b6b7d0d67c56aa9c01261ef2c
Justice Marks <her@shedaddy.com>
2019-06-09 03:25:21 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -136,8 +136,8 @@ def adding_to_dennis
         }
      }
      
-     programmer_hash[:dennis_ritchie][:languages].push("Assembly")
+     programmer_hash[:dennis_ritchie][:languages][1]= "Assembly"
      
-    
+    return programmer_hash
 
 end
